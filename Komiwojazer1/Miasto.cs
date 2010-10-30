f8b613c3720b149d9303accde97aabd03468c81c
pawel.wiejak <pawel.wiejak@1d43732d-b191-2424-b76e-da09e400fe3b>
2010-10-30 20:17:29 +0000

    wczytywanie miast i rysowanie ich na bitmapie

---
+++ Komiwojazer1/Miasto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace Komiwojazer1
+{
+    class Miasto
+    {
+        public Point wspolrzedneMiasta;        
+        //int wspolrzednaX, wspolrzednaY;
+
+        public Miasto(int x, int y)
+        {
+            wspolrzedneMiasta = new Point(x, y);
+        }
+
+
+    }
+}
