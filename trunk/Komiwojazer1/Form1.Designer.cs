58c765f3d013a9e6cf4843cf04f95bfeb9b998a1
pawel.wiejak <pawel.wiejak@1d43732d-b191-2424-b76e-da09e400fe3b>
2010-10-30 20:17:29 +0000

    wczytywanie miast i rysowanie ich na bitmapie

---
+++ trunk/Komiwojazer1/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Komiwojazer1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btWczytajMiasta = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.cbMiasta = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btWczytajMiasta
+            // 
+            this.btWczytajMiasta.Location = new System.Drawing.Point(60, 33);
+            this.btWczytajMiasta.Name = "btWczytajMiasta";
+            this.btWczytajMiasta.Size = new System.Drawing.Size(175, 23);
+            this.btWczytajMiasta.TabIndex = 0;
+            this.btWczytajMiasta.Text = "Wczytaj Miasta";
+            this.btWczytajMiasta.UseVisualStyleBackColor = true;
+            this.btWczytajMiasta.Click += new System.EventHandler(this.btWczytajMiasta_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(302, 33);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(480, 480);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // cbMiasta
+            // 
+            this.cbMiasta.FormattingEnabled = true;
+            this.cbMiasta.Location = new System.Drawing.Point(60, 91);
+            this.cbMiasta.Name = "cbMiasta";
+            this.cbMiasta.Size = new System.Drawing.Size(121, 21);
+            this.cbMiasta.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(873, 526);
+            this.Controls.Add(this.cbMiasta);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btWczytajMiasta);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btWczytajMiasta;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox cbMiasta;
+    }
+}
+
---
+++ trunk/Komiwojazer1/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Komiwojazer1
+{
+    public partial class Form1 : Form
+    {
+        Graphics rysunek;
+        Pen rysik = new Pen(Color.Black, 2);
+
+        public Form1()
+        {
+            InitializeComponent();
+            rysunek = this.pictureBox1.CreateGraphics();  
+        }
+
+        private void btWczytajMiasta_Click(object sender, EventArgs e)
+        {
+            List<Miasto> miasta = new List<Miasto>();
+            Miasto dodawaneMiasto = new Miasto(0,0);
+            TextReader tr = new StreamReader("Miasta.txt");
+            string miasto;
+            int x, y, a;
+            while ((miasto = tr.ReadLine()) != null)
+            {
+                cbMiasta.Items.Add(miasto);
+                a = miasto.IndexOf(" ");
+                int.TryParse( miasto.Substring(a+1, 3), out x);
+                int.TryParse(miasto.Substring(a + 5), out y);
+                dodawaneMiasto.wspolrzedneMiasta.X = x;
+                dodawaneMiasto.wspolrzedneMiasta.Y = y;
+                miasta.Add(dodawaneMiasto);
+                Rectangle kropka = new Rectangle(x, y, 2, 2);
+                rysunek.DrawRectangle(rysik, kropka);
+                
+            }
+            tr.Close();
+
+        }
+    }
+}
